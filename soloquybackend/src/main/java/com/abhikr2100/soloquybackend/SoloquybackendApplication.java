9864189fa2d568aa1ac1e8ad4a80405cb2edbf28
Abhishek Kumar <abhi.kr.2100@gmail.com>
2026-05-10 17:51:56 +0530

    feat(backend): add Spring Boot backend with hello endpoint

---
+++ soloquybackend/src/main/java/com/abhikr2100/soloquybackend/SoloquybackendApplication.java
@@ -0,0 +1,13 @@
+package com.abhikr2100.soloquybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloquybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloquybackendApplication.class, args);
+	}
+
+}
---
+++ soloquybackend/src/test/java/com/abhikr2100/soloquybackend/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.abhikr2100.soloquybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloReturnsExpectedMessage() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"message\": \"Hello, world!\"}"));
+    }
+}
